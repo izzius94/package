85bd51c866f404805b817ac1787939513ee00f12
izzius94 <izzius94@outlook.com>
2025-02-20 16:02:05 +0100

    feat: created blueprint for Decimal

--- src/Decimal.php
+++ src/Decimal.php
@@ -2,6 +2,35 @@
 
 namespace Izzius\Package;
 
-class Decimal
+readonly class Decimal
 {
+
+    /**
+     * @param int $value The value without comma
+     * @param int $comma The position of the comma
+     */
+    protected function __construct(public int $value, public int $comma)
+    {
+    }
+
+    /**
+     * Generate a new instance with a float
+     *
+     * @param float $number
+     * @return Decimal
+     */
+    public static function fromFloat(float $number): Decimal
+    {
+        return new Decimal(0, 0);
+    }
+
+    /**
+     * Get the original float value
+     *
+     * @return float
+     */
+    public function original(): float
+    {
+        return 0;
+    }
 }
