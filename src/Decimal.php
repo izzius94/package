d0ff26ce436c1793c43e77a82c645d7ce656036f
Maurizio Urso <izzius94@outlook.com>
2025-02-20 16:45:50 +0100

    Feat/create decimal handler (#1)
    * feat: created blueprint for Decimal
    
    * test: added test for Decimal
    
    * feat: added logic to generate a Decimal from a float
    
    * feat: added logic to get the original float value
    
    * style: cs-fixer
    
    * fix: phpstan
    
    * ci: added comment to PR with code coverage
    
    * ci: added slack notification
    
    * ci: fixed job status
    
    * ci: fixed pipeline

--- src/Decimal.php
+++ src/Decimal.php
@@ -2,6 +2,38 @@
 
 namespace Izzius\Package;
 
-class Decimal
+readonly class Decimal
 {
+    /**
+     * @param int $value The value without comma
+     * @param int $comma The position of the comma
+     */
+    protected function __construct(public int $value, public int $comma)
+    {
+    }
+
+    /**
+     * Generate a new instance with a float
+     *
+     * @param float $number
+     * @return Decimal
+     */
+    public static function fromFloat(float $number): Decimal
+    {
+        $converted = (string)$number;
+        $comma = strpos($converted, '.');
+        $value = (int)str_replace('.', '', $converted);
+
+        return new Decimal($value, $comma);
+    }
+
+    /**
+     * Get the original float value
+     *
+     * @return float
+     */
+    public function original(): float
+    {
+        return (float)substr_replace((string)$this->value, '.', $this->comma, 0);
+    }
 }
--- tests/Unit/DecimalTest.php
+++ tests/Unit/DecimalTest.php
@@ -2,12 +2,24 @@
 
 namespace Izzius\Package\Test\Unit;
 
+use Izzius\Package\Decimal;
 use PHPUnit\Framework\TestCase;
 
 class DecimalTest extends TestCase
 {
-    public function testDecimal()
+    public function testCanCreateADecimalFromAFloat()
     {
-        $this->assertTrue(true);
+        $decimal = Decimal::fromFloat(10.808);
+
+        $this->assertEquals(10808, $decimal->value);
+        $this->assertEquals(2, $decimal->comma);
+    }
+
+    public function testCanGetTheOriginalValue()
+    {
+        $original = 10.808;
+        $decimal = Decimal::fromFloat($original);
+
+        $this->assertEquals($original, $decimal->original());
     }
 }
